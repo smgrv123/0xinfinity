334c7a2ad58e0b9068f03f50003e6bb1820781d4
Swarga-codes <sd3526@srmist.edu.in>
2022-05-12 01:39:36 +0530

    header done(functionality left)

---
+++ src/components/FAQ.jsx
@@ -0,0 +1,13 @@
+import './FAQ.css';
+const FAQ = () => {
+    return ( 
+        <div className="FAQ">
+        <div className="FAQHeadings">
+        <p>FAQs</p>
+        <h1>FREQUENTLY ASKED<br />QUESTI<span>O</span>NS</h1>
+        </div>
+        </div>
+     );
+}
+ 
+export default FAQ;
