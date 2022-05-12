3039d87682148fabb23a8bd9d2f56d63ae43a900
Swarga-codes <sd3526@srmist.edu.in>
2022-05-12 21:07:44 +0530

    faq done

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -6,6 +6,20 @@ const FAQ = () => {
         <p>FAQs</p>
         <h1>FREQUENTLY ASKED<br />QUESTI<span>O</span>NS</h1>
         </div>
+        <div className="FAQQuestions">
+        <div className="QuestOne">
+        <p>Who is it for?</p>
+        </div>
+        <div className="QuestOne">
+        <p>What service we will provide?</p>
+        </div>
+        <div className="QuestOne">
+        <p>How much do we charge?</p>
+        </div>
+        <div className="QuestOne">
+        <p>How do we select a project?</p>
+        </div>
+        </div>
         </div>
      );
 }
