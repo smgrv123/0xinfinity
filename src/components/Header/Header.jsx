d544c95b1368c7c80faaec866d8ea4dbf64b1dc4
Swarga-codes <sd3526@srmist.edu.in>
2022-05-21 16:18:23 +0530

    bouncy anim left

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,40 @@
+import React, {useEffect} from 'react'
 import './Header.css'
 import Studio from '../../assets/Studio-Project 2 - Copy.png'
+const useMousePosition = () => {
+    const [mousePosition, setMousePosition] = React.useState({x:0, y:0})
+    useEffect(()=>{
+      const updateMousePosition = event => {
+        setMousePosition({x: event.clientX, y: event.clientY})
+        
+      }    
+     window.addEventListener('mousemove',updateMousePosition);
+      return () => window.removeEventListener('mousemove', updateMousePosition)
+    },[])
+    
+      return mousePosition   
+}
 const Header = () => {
+ 
+      
+      let bannerimg = document.querySelector('#banner_head');
+      const {x,y} = useMousePosition();
+      console.log(x,y);
+      if(x>0 && x<550  ){
+        bannerimg.style.transform = 'translateX(10rem)';
+        bannerimg.style.transition = '3s';
+        bannerimg.style.animation = 'bounce 0.5s';
+        // bannerimg.style.overflow="hidden";
+      }
+     else if(x>765&& x<1000){
+      bannerimg.style.transform = 'translateX(-10rem)';
+      bannerimg.style.transition = '3s';
+      bannerimg.style.animation = 'bounce 0.5s';
+    //   bannerimg.style.overflowX="hidden";
+     }
     return (
         <div className="Header">
-            <h1><span>0x</span>INFINITY LABS</h1>
+            <h1 id='banner_head'><span>0x</span>INFINITY LABS</h1>
             <p>Emp<span>o</span>wering Creat<span>o</span>rs</p>
             <button className="regBtn">Register</button>
             <img src={Studio} alt="" className='Headerbg' />
