51bd80366dfc33dc44338d6a4b34d432bba05d7a
Sumrit Grover <sumritgrover1@gmail.com>
2022-06-02 18:11:24 +0530

    CSS errors fixed, animation issue solved and modal fix

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import './App.css';
-import About from './components/About/About';
-import Goal from './components/Goal/Goal';
-import Timeline from './components/Timeline1/Timeline';
-import Timeline2 from './components/Timeline2/Timeline2';
-import FAQ from './components/FAQ/FAQ';
-import Header from './components/Header/Header';
-import Navbar from './components/Navbar/Navbar';
-import Footer from './components/Footer/Footer';
-import ComingSoon from './components/Coming Soon/ComingSoon';
-import CarouselSec from './components/Carousel/CarouselSec';
+import "./App.css";
+import About from "./components/About/About";
+import Goal from "./components/Goal/Goal";
+import Timeline from "./components/Timeline1/Timeline";
+import Timeline2 from "./components/Timeline2/Timeline2";
+import FAQ from "./components/FAQ/FAQ";
+import Header from "./components/Header/Header";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+import ComingSoon from "./components/Coming Soon/ComingSoon";
+import CarouselSec from "./components/Carousel/CarouselSec";
+
 function App() {
   return (
-
     <main>
       <Navbar />
       <Header />
@@ -26,4 +26,5 @@ function App() {
     </main>
   );
 }
+
 export default App;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,77 +1,85 @@
-import React, { useEffect } from 'react'
-import './Header.css'
-import Studio from '../../assets/Studio-Project 2 - Copy.png'
+import React, { useEffect, useState } from "react";
 import Popup from "reactjs-popup";
 import "reactjs-popup/dist/index.css";
-import PopImg from '../../assets/formimg.png';
+
+import "./Header.css";
+import Studio from "../../assets/Studio-Project 2 - Copy.png";
+import PopImg from "../../assets/formimg.png";
+import Modal from "../Modal/Modal";
+// import { isVisible } from "@testing-library/user-event/dist/utils";
+
 const useMousePosition = () => {
-  const [mousePosition, setMousePosition] = React.useState({ x: 0, y: 0 })
+  const [mousePosition, setMousePosition] = React.useState({ x: 0, y: 0 });
+
   useEffect(() => {
-    const updateMousePosition = event => {
-      setMousePosition({ x: event.clientX, y: event.clientY })
+    const updateMousePosition = (event) => {
+      setMousePosition({ x: event.clientX, y: event.clientY });
+    };
 
-    }
-    window.addEventListener('mousemove', updateMousePosition);
-    return () => window.removeEventListener('mousemove', updateMousePosition)
-  }, [])
+    window.addEventListener("mousemove", updateMousePosition);
 
-  return mousePosition
-}
+    return () => window.removeEventListener("mousemove", updateMousePosition);
+  }, []);
+
+  return mousePosition;
+};
+
 const Header = () => {
 
+  const [isVisible, setisVisible] = useState(false);
 
-  let bannerimg = document.querySelector('#banner_head');
+
+
+  let bannerimg = document.querySelector("#banner_head");
+  
   const { x, y } = useMousePosition();
-  console.log(x, y);
-  if (x > 0 && x < 550) {
-    bannerimg.style.transform = 'translateX(10rem)';
-    bannerimg.style.transition = '3s';
-    bannerimg.style.animation = 'bounce 0.5s';
-    // bannerimg.style.overflow="hidden";
-  }
-  else if (x > 765 && x < 1000) {
-    bannerimg.style.transform = 'translateX(-10rem)';
-    bannerimg.style.transition = '3s';
-    bannerimg.style.animation = 'bounce 0.5s';
-    //   bannerimg.style.overflowX="hidden";
+  
+  if (window.screen.width > 881) {
+    if (x > 0 && x < 550) {
+      bannerimg.style.transform = "translateX(10rem)";
+      bannerimg.style.transition = "3s";
+      bannerimg.style.animation = "bounce 0.5s";
+      // bannerimg.style.overflow="hidden";
+    } else if (x > 765 && x < 1000) {
+      bannerimg.style.transform = "translateX(-10rem)";
+      bannerimg.style.transition = "3s";
+      bannerimg.style.animation = "bounce 0.5s";
+      //   bannerimg.style.overflowX="hidden";
+    }
   }
   return (
     <div className="Header">
-      <h1 id='banner_head'><span>0x</span>INFINITY LABS</h1>
-      <p>Emp<span>o</span>wering Creat<span>o</span>rs</p>
-
-      <Popup trigger={<button className="regBtn">Register</button>} modal nested>
-        {() => (
-          <form>
-            <div className="Popup">
-              <div className="popleft">
-                <div className="popup-heading">
-                  <h1>Got interesting ideas for your next NFT project?</h1>
-                </div>
-                <div className="popForm">
-                  <label htmlFor="">Your Name</label>
-                  <input type="text" />
-                  <label htmlFor="">Your Email Address</label>
-                  <input type="text" />
-                  <label htmlFor="">Tell us a bit about yourself and your idea...</label>
-                  <textarea rows="5" cols="74" name="comment" ></textarea>
-                  {/* <input type="text" /> */}
-                  <label htmlFor="">Attach your files here</label>
-                  <input type="text" />
-                  <button className='submit'>Submit</button>
-                  <button className='cancel'>Cancel</button>
-                </div>
-              </div>
-              <div className="popright">
-                <img src={PopImg} alt="" />
-              </div>
-            </div>
-          </form>
-        )}
-      </Popup>
-      <img src={Studio} alt="" className='Headerbg' />
+      <h1 id="banner_head">
+        <span>0x</span>INFINITY LABS
+      </h1>
+      <p>
+        Emp<span>o</span>wering Creat<span>o</span>rs
+      </p>
+      <button
+        className="regBtn"
+        onClick={() => {
+          console.log("first");
+          setisVisible(true);
+        }}
+      >
+        Register
+      </button>
+      {isVisible ? (
+        <div
+          className="popup"
+          // trigger={<button className="regBtn">Register</button>}
+          // modal
+          // nested
+          // position={'center'}
+        >
+          {/* {() => ( */}
+          <Modal setisVisible={setisVisible} />
+          {/* )} */}
+        </div>
+      ) : null}
+      <img src={Studio} alt="" className="Headerbg" />
     </div>
   );
-}
+};
 
 export default Header;
