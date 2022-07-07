f43fd8311ade5f8599c827f474ee69fc7370da6c
shubhraagarwal <agarwalshubhra2002@gmail.com>
2022-07-07 19:53:50 +0530

    :lipstick: Fixed carousel responsive issue

--- src/components/Carousel/CarouselSec.jsx
+++ src/components/Carousel/CarouselSec.jsx
@@ -1,30 +1,31 @@
-import React from 'react'
-import Carousel from 'react-multi-carousel';
+import React from "react";
+import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
-import './CarouselSec.css';
+import "./CarouselSec.css";
 
 const responsive = {
   superLargeDesktop: {
     breakpoint: { max: 4000, min: 3000 },
-    items: 3
+    items: 3,
   },
   desktop: {
     breakpoint: { max: 3000, min: 1024 },
-    items: 3.5
+    items: 3.5,
   },
   tablet: {
     breakpoint: { max: 1024, min: 991 },
-    items: 4
+    items: 4,
   },
   mobile: {
     breakpoint: { max: 991, min: 0 },
-    items: 2.5
-  }
+    items: 2.5,
+  },
 };
 const CarouselSec = () => {
   return (
     <div className="Carousel" id="ab">
-      <Carousel responsive={responsive}
+      <Carousel
+        responsive={responsive}
         swipeable={true}
         draggable={true}
         arrows={false}
@@ -32,20 +33,20 @@ const CarouselSec = () => {
         shouldResetAutoplay={false}
         infinite={true}
       >
-        <div className='unselectable'>consultation</div>
-        <div className='unselectable'>incubation</div>
-        <div className='unselectable'>advice</div>
-        <div className='unselectable'>project</div>
-        <div className='unselectable'>management</div>
-        <div className='unselectable'>marketing</div>
-        <div className='unselectable'>smart</div>
-        <div className='unselectable'>contract</div>
-        <div className='unselectable'>development</div>
-        <div className='unselectable'>tokenomics</div>
-        <div className='unselectable'>audit</div>
+        <div className="unselectable">consultation</div>
+        <div className="unselectable">incubation</div>
+        <div className="unselectable">advice</div>
+        <div className="unselectable">project</div>
+        <div className="unselectable">management</div>
+        <div className="unselectable">marketing</div>
+        <div className="unselectable">smart</div>
+        <div className="unselectable">contract</div>
+        <div className="unselectable">development</div>
+        <div className="unselectable">tokenomics</div>
+        <div className="unselectable">audit</div>
       </Carousel>
     </div>
   );
-}
+};
 
 export default CarouselSec;
